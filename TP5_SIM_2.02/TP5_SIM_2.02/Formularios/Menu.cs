18b778b474e812a32af17b984808128f9f08c9a0
dlizarralde2020 <dlizarralde2020@hotmail.com>
2020-06-02 12:00:56 -0300

    cambios en el diccionario clientes

--- TP5_SIM_2.02/TP5_SIM_2.02/Formularios/Menu.cs
+++ TP5_SIM_2.02/TP5_SIM_2.02/Formularios/Menu.cs
@@ -50,8 +50,6 @@ namespace TP5_SIM_2._02.Formularios
             string estado_caja_2 = "";
             int cola_caja_2 = 0;
             int cant_clientes_finalizados = 0;
-            double tiempo_ocioso_caja_1 = 0;
-            int cant_caja_2_usada = 0;
 
             double rnd_lleg_cliente = random.NextDouble();
             double rnd_fin_at = 0;
@@ -63,11 +61,16 @@ namespace TP5_SIM_2._02.Formularios
             double[] acum_tiempo_at;
             acum_tiempo_at = new double[100];
 
-            int[] acum_clientes_at_finalizada;
-            acum_clientes_at_finalizada = new int[100];
+            int acum_clientes_at_finalizada = 0;
+
+            double[] tiempo_ocioso_caja_1;
+            tiempo_ocioso_caja_1 = new double[100];
+
+            int[] cant_caja_2_usada;
+            cant_caja_2_usada = new int[100];
 
             // i = filas
-            for (int i = 0; acum_clientes_at_finalizada[i] < 11; i++)
+            for (int i = 0; acum_clientes_at_finalizada < 11; i++)
             {
 
                 // calculo estado caja 2
@@ -89,18 +92,18 @@ namespace TP5_SIM_2._02.Formularios
                     nombre_evento = "Inicializacion";
                     reloj = 0.00;
                     acum_tiempo_at[i] = 0;
-                    acum_clientes_at_finalizada[i] = 0;
                     rnd_lleg_cliente = random.NextDouble();
                     tiempo_entre_llegadas = -media * Math.Log(1 - rnd_lleg_cliente);
                     tiempo_atencion = tiempo_entre_llegadas + reloj;
 
-                    dgv_datos.Rows.Add(nombre_evento, reloj, rnd_lleg_cliente, tiempo_entre_llegadas, tiempo_atencion, rnd_fin_at, tiempo_fin_atencion, rnd_pago, metodo_pago, fin_at_caja_1, fin_at_caja_2, estado_caja_1, cola_caja_1, estado_caja_2, cola_caja_2, acum_tiempo_at[i], acum_clientes_at_finalizada[i], tiempo_ocioso_caja_1, cant_caja_2_usada);
+                    dgv_datos.Rows.Add(nombre_evento, reloj, rnd_lleg_cliente, tiempo_entre_llegadas, tiempo_atencion, rnd_fin_at, tiempo_fin_atencion, rnd_pago, metodo_pago, fin_at_caja_1, fin_at_caja_2, estado_caja_1, cola_caja_1, estado_caja_2, cola_caja_2, acum_tiempo_at[i], acum_clientes_at_finalizada, tiempo_ocioso_caja_1, cant_caja_2_usada);
                 }
                 // a partir de fila = 1 ->   eventos tienen nombre
                 else
                 {
                     // calculo proximo evento
                     // tiempo_atencion, fin_at_caja_1, fin_at_caja_2  ->  valores vienen de la fila anterior
+                    // por eso se vuelven a calcular mas abajo
                     // nombre_evento, reloj  ->  fila actual
                     // por cada 'llegada_cliente' agrego un cliente 'cli' al diccionario, lo que cambia es el indice (x) de ese diccionario.
                     // si es la fila 1 se crea el cliente con todos sus atributos 
@@ -110,6 +113,7 @@ namespace TP5_SIM_2._02.Formularios
                         nombre_evento = "llegada_cliente";
                         reloj = tiempo_atencion;
 
+                        // x contador personal del cliente 
                         x = x + 1;
                         clientes.Add(x, cli);
                         // si es la primer fila o el dicionario clientes no tiene ninguna cliente guardado, el cliente nuevo se guarda en el indice 1
@@ -132,23 +136,34 @@ namespace TP5_SIM_2._02.Formularios
                             // pasa al indice 1 el que tenia indice 2 y su estado cambia a "SAC"
                             reloj = fin_at_caja_1;
                             nombre_evento = "fin_atencion_caja_1";
-                            clientes.Remove(1);
-                            clientes[1].estado = "SAC";
+
+                            KeyValuePair<int, Cliente> primer_valor = clientes.First();
+                            clientes.Remove(primer_valor.Key);
+
+                            if (cola_caja_1 > 0)
+                            {
+                                KeyValuePair<int, Cliente> primer_valor_actual = clientes.First();
+                                clientes[primer_valor_actual.Key].estado = "SAC";
+                            }                      
+                        
                             x = x - 1;
                             cola_caja_1 = cola_caja_1 - 1;
+                            acum_clientes_at_finalizada = acum_clientes_at_finalizada + 1;
                         }
                         else
                         {
                             reloj = fin_at_caja_2;
                             nombre_evento = "fin_atencion_caja_2";
+
+                            acum_clientes_at_finalizada = acum_clientes_at_finalizada + 1;
                         }
                     }
 
                     // todos estos datos son de la fila actual
                     // se calcula toda las columnas de "llegada cliente" y "fin atencion caja (i)"
                     // ya sea cuando la caja con cola se desocupa y se atiende al siguiente en cola
-                    // o cuando llega un nuevo cliente
-                    if ((nombre_evento == "fin_atencion_caja_1" && cola_caja_1 > 0) || nombre_evento == "llegada_cliente")
+                    // o cuando llega un nuevo cliente y no hay nadie en cola
+                    if ((nombre_evento == "fin_atencion_caja_1" && cola_caja_1 > 0) || (nombre_evento == "llegada_cliente" && cola_caja_1 == 0))
                     {
                         estado_caja_1 = "Ocupado";
 
@@ -188,18 +203,7 @@ namespace TP5_SIM_2._02.Formularios
                         acum_tiempo_at[i] = acum_tiempo_at[i - 1];
                     }
 
-                    // acumulador cantidad de clientes con atencion finalizada
-                    if (nombre_evento == "fin_atencion_caja_2" || nombre_evento == "fin_atencion_caja_1")
-                    {
-                        acum_clientes_at_finalizada[i] = acum_clientes_at_finalizada[i - 1] + 1;
-                    }
-                    else
-                    {
-                        acum_clientes_at_finalizada[i] = acum_clientes_at_finalizada[i - 1];
-                    }
-
-
-                    dgv_datos.Rows.Add(nombre_evento, reloj, rnd_lleg_cliente, tiempo_entre_llegadas, tiempo_atencion, rnd_fin_at, tiempo_fin_atencion, rnd_pago, metodo_pago, fin_at_caja_1, fin_at_caja_2, estado_caja_1, cola_caja_1, estado_caja_2, cola_caja_2, acum_tiempo_at[i], acum_clientes_at_finalizada[i], tiempo_ocioso_caja_1, cant_caja_2_usada);
+                    dgv_datos.Rows.Add(nombre_evento, reloj, rnd_lleg_cliente, tiempo_entre_llegadas, tiempo_atencion, rnd_fin_at, tiempo_fin_atencion, rnd_pago, metodo_pago, fin_at_caja_1, fin_at_caja_2, estado_caja_1, cola_caja_1, estado_caja_2, cola_caja_2, acum_tiempo_at[i], acum_clientes_at_finalizada, tiempo_ocioso_caja_1[i], cant_caja_2_usada[i]);
 
 
                 }
